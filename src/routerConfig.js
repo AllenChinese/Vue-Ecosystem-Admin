90e6cee40ce267a0ca98b46bba50c8a229e4e1e7
AllenChinese <13402202685@163.com>
2019-04-13 12:23:49 +0800

    feat: Echarts base example component

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -1,12 +1,19 @@
 // 以下文件格式是描述路由的协议格式
 // 布局
-const HeaderAsideLayout = () => import(/* webpackChunkName: "HeaderAsideLayout" */ './layouts/HeaderAsideLayout')
-const EmptyLayout = () => import(/* webpackChunkName: "EmptyLayout" */ './layouts/EmptyLayout')
+const HeaderAsideLayout = () =>
+  import(/* webpackChunkName: "HeaderAsideLayout" */ './layouts/HeaderAsideLayout');
+const EmptyLayout = () =>
+  import(/* webpackChunkName: "EmptyLayout" */ './layouts/EmptyLayout');
 // 页面
-const FormMaking = () => import(/* webpackChunkName: "FormMaking" */ './views/FormMaking')
-const FormGenerator = () => import(/* webpackChunkName: "FormGenerator" */ './views/FormGenerator')
+const FormMaking = () =>
+  import(/* webpackChunkName: "FormMaking" */ './views/FormMaking');
+const FormGenerator = () =>
+  import(/* webpackChunkName: "FormGenerator" */ './views/FormGenerator');
+const Echarts = () =>
+  import(/* webpackChunkName: "Echarts" */ './views/Echarts');
 // 404
-const NotFound = () => import(/* webpackChunkName: "NotFound" */ './views/NotFound')
+const NotFound = () =>
+  import(/* webpackChunkName: "NotFound" */ './views/NotFound');
 
 const routeConfig = [
   {
@@ -30,10 +37,24 @@ const routeConfig = [
     ]
   },
   {
+    path: '/dataVisual',
+    layout: HeaderAsideLayout,
+    component: Echarts,
+    name: 'dataVisual',
+    children: [
+      {
+        path: '/dataVisual/echarts',
+        layout: HeaderAsideLayout,
+        component: Echarts,
+        name: 'Echarts'
+      }
+    ]
+  },
+  {
     path: '*',
     layout: EmptyLayout,
-    component: NotFound,
+    component: NotFound
   }
-]
+];
 
-export default routeConfig
+export default routeConfig;
---
+++ src/views/Echarts/Echarts.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="echarts-wrapper">
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content">
+          <div
+            id="stackedArea"
+            :style="{width: '680px', height: '380px'}"
+          ></div>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content-light">
+          <div
+            id="rainFall"
+            :style="{width: '680px', height: '380px'}"
+          ></div>
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+import Echarts from 'echarts';
+import stackedAreaOption from './stackedArea';
+import rainFallOption from './rainFall';
+export default {
+  name: 'Echarts',
+  mounted() {
+    this.drawStackedArea();
+    this.drawRainFall();
+  },
+  methods: {
+    drawStackedArea() {
+      // 基于准备好的dom，初始化echarts实例
+      let stackedArea = Echarts.init(document.getElementById('stackedArea'));
+      stackedArea.setOption(stackedAreaOption);
+    },
+
+    drawRainFall() {
+      let rainFall = Echarts.init(document.getElementById('rainFall'));
+      rainFall.setOption(rainFallOption);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.echarts-wrapper {
+  padding: 30px 0;
+}
+</style>
+
